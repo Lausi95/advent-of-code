01444c7cdeffa9bed2cabe3e464a5c25c212b1e4
Tom Lausmann <tomlausmann@gmail.com>
2023-12-25 22:31:42 +0100

    Solve Day04

--- src/main/kotlin/day04/Day04.kt
+++ src/main/kotlin/day04/Day04.kt
@@ -2,14 +2,47 @@ package day04
 
 import java.nio.file.Files
 import java.nio.file.Path
+import kotlin.math.pow
 import kotlin.system.measureTimeMillis
 
+data class Card(val cardNumber: Int, val winningNumbers: List<Int>, val numbers: List<Int>, var instances: Int = 1) {
+  companion object {
+    fun parse(str: String): Card {
+      val split1 = str.split(":")
+      val cardNumber = split1[0].split(" ").filter { it.isNotBlank() }[1].toInt()
+
+      val split2 = split1[1].split("|")
+      val winningNumbers = split2[0].split(" ").filter { it.isNotBlank() }.map { it.toInt() }
+      val numbers = split2[1].split(" ").filter { it.isNotBlank() }.map { it.toInt() }
+
+      return Card(cardNumber, winningNumbers, numbers)
+    }
+  }
+
+  fun score(): Int {
+    val amount = matches()
+    if (amount == 0)
+      return 0
+    return 2.0.pow(amount.toDouble() - 1).toInt()
+  }
+
+  fun matches(): Int = winningNumbers.count { numbers.contains(it) }
+}
+
 fun solvePart1(input: List<String>): Int {
-  return 0
+  return input.map(Card::parse).sumOf(Card::score)
 }
 
 fun solvePart2(input: List<String>): Int {
-  return 0
+  val cards = input.map(Card::parse)
+  for (card in cards) {
+    for (x in 1..card.matches()) {
+      cards.find { c -> c.cardNumber == card.cardNumber + x }?.let {
+        it.instances += 1 * card.instances
+      }
+    }
+  }
+  return cards.sumOf { it.instances }
 }
 
 fun <T> measure(name: String, fn: () -> T): T {
@@ -24,6 +57,8 @@ fun <T> measure(name: String, fn: () -> T): T {
 fun main() {
   val testData = Files.readAllLines(Path.of("./input/04/input.test.txt"))
   val data = Files.readAllLines(Path.of("./input/04/input.txt"))
+
+  measure("Warmup") { solvePart1(testData) }
 
   measure("Part 1 Test") { solvePart1(testData) }
   measure("Part 1     ") { solvePart1(data) }
