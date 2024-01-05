6c1edf0752b6e3ca893711239df9cc00da4c6355
Tom Lausmann <tom.lausmann@unitb.de>
2024-01-05 17:35:33 +0100

    Add Day12 solution skeleton

---
+++ src/main/kotlin/day12/Day12.kt
@@ -0,0 +1,9 @@
+package day12
+
+import solve
+
+private fun solvePart1(input: List<String>): Long = 0
+
+private fun solvePart2(input: List<String>): Long = 0
+
+private fun main() = solve("11", ::solvePart1, ::solvePart2)
